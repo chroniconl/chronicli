207533ca26e062084b92db42aae473ab85eeccd5
matéo! <6matbub@gmail.com>
2024-02-13 04:46:10 -0800

    feat: implement cli interface to make things friendly

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Build Chroniconl 
 
 This is a temp solution cli for my blog https://chroniconl.com  
+
+
+### Commands
+
+#### 1. Clean Articles Directory
+
+- **Command:** `node index clean`
+- **Description:** Cleans the articles directory by removing all its contents.
+- **Action:** Executes the `cleanArticlesDirectory` function, which presumably deletes all files or performs necessary cleanup operations in the articles directory.
+
+#### 2. Fetch All Articles
+
+- **Command:** `node index fetch`
+- **Description:** Fetches all articles without including bulky content.
+- **Action:** Calls the `getPostsWithoutBulkyContent` function, which returns a list of articles excluding bulky content. The fetched articles are then logged to the console.
+
+#### 3. Build Articles
+
+- **Command:** `node index build [options]`
+- **Description:** Builds articles based on the specified options.
+- **Options:**
+    - `-a, --all`: Build all articles. This option triggers the `buildAllArticles` function, which presumably compiles or processes all articles.
+    - `-i, --id`: Build an article by its ID. This option requires specifying the article ID and triggers the `buildArticleById` function, which builds the specified article.
+- **Action:** Depending on the options provided, it either builds all articles or a specific article by ID. If both options are specified simultaneously, it logs a message asking to specify only one option at a time.
+
+#### Default Action
+
+- **Action:** If no command is specified, the program logs a message indicating that no command was provided and suggests using `--help` to see available commands and options.
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /**
  * This is a pretty raw way of making a static blog
  * basically this is a script that is manually executed
@@ -13,6 +15,10 @@ const fs = require('fs');
 const path = require('path');
 const {supabase} = require('./utils/supabase');
 const template = require('./utils/template');
+const { program } = require('commander');
+
+// App version
+program.version('0.1.0');
 
 /**
  * 
@@ -60,6 +66,34 @@ async function getPosts() {
 	}
 
   	return data;
+}
+
+// Read a single row in the `posts` table
+async function getPostById(id) {
+	// TODO Fix this
+	// const {data, error} = await supabase
+	// 	.from('posts')
+	// 	.select('*')
+	// 	.eq('id', id)
+
+	// if (error) {
+	// 	throw error;
+	// }
+
+	// return data;
+}
+
+// Read all rows in the `posts` table without the `content` column
+async function getPostsWithoutBulkyContent() {
+	const {data, error} = await supabase
+		.from('posts')
+		.select('id, title, slug, author, created_at, updated_at')
+
+	if (error) {
+		throw error;
+	}
+
+	return data;
 }
 
 /**
@@ -133,8 +167,8 @@ function buildFile (slug, content) {
 	}
 }
 
-// Main function
-function main () {
+// Build all articles function
+function buildAllArticles () {
 	try {
 		// Clean the articles directory
 		cleanArticlesDirectory();
@@ -173,4 +207,95 @@ function main () {
 	}
 }
 
-main();
+// Build article by id function
+function buildArticleById (id) {
+	try {
+		// Fetch the post from the database
+		getPostById(id).then(posts => {
+			const post = posts[0];
+			// Generate the MDX file for the post
+			if (post) {
+				// Format the slug of the post
+				const articleSlug = formatSlug(post.slug || post.title);
+
+				// make directory based on the slug of the post
+				// if it doesn't already exist
+				buildDirectory(articleSlug);
+
+				// Generate the content for the MDX file
+				const content = template({
+					author: post.author, 
+					date: post.created_at, 
+					lastUpdated: post.updated_at,
+					title: post.title, 
+					seoDescription: post.seo_description,
+					content: post.content,
+					tags: post.tags,
+					filterableTags: post.filterable_tabs	
+				});
+
+				// Write the file to the `articles/${slug}/` directory
+				// with the slug as the filename
+				buildFile(articleSlug, content);
+
+			}
+
+			if (!post) {
+				console.log('No post found with that id.');
+				return;
+			}
+		});
+	}
+	catch (error) {
+		console.error(error);
+	}
+}
+
+program
+	.command('clean')
+	.description('Clean articles directory')
+	.action(() => {
+		console.log('Cleaning articles directory...');
+		cleanArticlesDirectory();
+	});
+
+program
+	.command('fetch')
+	.description('Fetch all articles')
+	.action(() => {
+		
+		getPostsWithoutBulkyContent().then(posts => {			
+			console.log(posts);
+
+		});
+	});
+
+program
+  .command('build')
+  .description('Build all articles')
+  .option('-a, --all', 'Build [all] articles')
+	.option('-i, --id', 'Build article by [id]')
+  .action((options) => {
+		if (options.all && options.id) {
+			console.log('Please specify only one option at a time.');
+			return;
+		}
+		
+		if (options.all) {
+			console.log('Building all articles...');
+			buildAllArticles();
+		}
+		
+		else if (options.id) {
+			console.log('Building article by id...');
+			buildArticleById(options.id);
+		}
+	});
+
+program
+  .action(function () {
+    console.log('No command specified. Use --help to see available commands and options.');
+  });
+
+// Parse the command line arguments
+program.parse(process.argv);
